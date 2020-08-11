c76a28f74f071e4d02bd8f0ea56d63c4e786e4e8
SM <hcooch2ch3@gmail.com>
2020-08-11 21:51:35 +0900

    Initial Commit

---
+++ FiveTwoFiveRule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FiveTwoFiveRule
+//
+//  Created by 임성민 on 2020/08/11.
+//  Copyright © 2020 SeongMin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
