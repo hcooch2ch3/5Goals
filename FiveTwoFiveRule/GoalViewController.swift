ea73d15887dfd03ac645696bdb2741bc367a5f2e
SM <hcooch2ch3@gmail.com>
2020-08-11 22:47:57 +0900

    Implement storyboard, add view controller, and add models
    - Implement basic structure of storyboard
    - Add goal, wish, givingup view controller
    - Add goal, wish, givingup model

--- FiveTwoFiveRule/GoalViewController.swift
+++ FiveTwoFiveRule/GoalViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class GoalViewController: UIViewController {
+    
+    @IBOutlet var goalTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
