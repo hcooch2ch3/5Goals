506469e11000ea3654408e8bc16f32d2cf5111b7
SM <hcooch2ch3@gmail.com>
2020-08-19 18:01:28 +0900

    Implement Help View

---
+++ FiveTwoFiveRule/Controller/HelpViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HelpViewController.swift
+//  FiveTwoFiveRule
+//
+//  Created by 임성민 on 2020/08/19.
+//  Copyright © 2020 SeongMin. All rights reserved.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func touchUpExitButton(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
