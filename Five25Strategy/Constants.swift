36ab99d6f241860a5057ba690cfc53cbd1bf359e
SM <hcooch2ch3@gmail.com>
2021-06-15 23:58:37 +0900

    Add enum Constants

---
+++ Five25Strategy/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Five25Strategy
+//
+//  Created by 임성민 on 2021/06/15.
+//  Copyright © 2021 SeongMin. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    enum PersistentContainer {
+        static let name = "Five25Strategy"
+        static let groupIdentifier = "group.SM.Five25Strategy"
+        static let pathComponent = "Five25Strategy.sqlite"
+    }
+}
