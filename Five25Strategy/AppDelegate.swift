66c3a826bcc56fe823efb1287348b64f1bd84f82
SM <hcooch2ch3@gmail.com>
2021-06-16 00:03:33 +0900

    Refactor CoreData code - Add and Implement PersistentContainer class - Remove CoreData code in AppDelegate

--- Five25Strategy/AppDelegate.swift
+++ Five25Strategy/AppDelegate.swift
@@ -12,7 +12,7 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    lazy var persistentContainer: NSPersistentContainer = PersistentContainer()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
@@ -32,85 +32,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-    // MARK: - Core Data stack
-
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
-        let container = NSPersistentContainer(name: "Five25Strategy")
-        let storeURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.SM.Five25Strategy")!.appendingPathComponent("Five25Strategy.sqlite")
-        
-        /// Check whether there is old data or not.
-        var defaultURL: URL?
-        if let storeDescription = container.persistentStoreDescriptions.first, let url = storeDescription.url {
-            defaultURL = FileManager.default.fileExists(atPath: url.path) ? url : nil
-        }
-    
-        if defaultURL == nil {
-            container.persistentStoreDescriptions = [NSPersistentStoreDescription(url: storeURL)]
-        }
-        
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-            
-            /// Migrate old data to new data store.
-            if let url = defaultURL, url.absoluteString != storeURL.absoluteString {
-                let coordinator = container.persistentStoreCoordinator
-                if let oldStore = coordinator.persistentStore(for: url) {
-                    do {
-                        try coordinator.migratePersistentStore(oldStore, to: storeURL, options: nil, withType: NSSQLiteStoreType)
-                    } catch {
-                        print(error.localizedDescription)
-                    }
-
-                    /// Delete old store.
-                    let fileCoordinator = NSFileCoordinator(filePresenter: nil)
-                    fileCoordinator.coordinate(writingItemAt: url, options: .forDeleting, error: nil, byAccessor: { url in
-                        do {
-                            try FileManager.default.removeItem(at: url)
-                        } catch {
-                            print(error.localizedDescription)
-                        }
-                    })
-                }
-            }
-        })
-        return container
-    }()
-
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
     }
 
 }
---
+++ Five25Strategy/Model/PersistentContainer.swift
@@ -0,0 +1,79 @@
+//
+//  PersistentContainer.swift
+//  Five25Strategy
+//
+//  Created by 임성민 on 2021/06/15.
+//  Copyright © 2021 SeongMin. All rights reserved.
+//
+
+import CoreData
+
+final class PersistentContainer: NSPersistentContainer {
+    
+    private let persistentStoreURL: URL? = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: Constants.PersistentContainer.groupIdentifier)?.appendingPathComponent(Constants.PersistentContainer.pathComponent)
+    
+    convenience init() {
+        self.init(name: Constants.PersistentContainer.name)
+        
+        guard let persistentStoreURL = self.persistentStoreURL else {
+            return
+        }
+
+        if let previousStoreURL = persistentStoreDescriptions.first?.url,
+           FileManager.default.fileExists(atPath: previousStoreURL.path) &&
+            previousStoreURL.absoluteString != persistentStoreURL.absoluteString {
+            migrate(from: previousStoreURL, to: persistentStoreURL)
+            return
+        }
+
+        persistentStoreDescriptions = [NSPersistentStoreDescription(url: persistentStoreURL)]
+        loadPersistentStores { persistentStoreDescription, error in
+            if let error = error as NSError? {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func migrate(from oldStoreURL: URL, to newStoreURL: URL) {
+        loadPersistentStores { persistentStoreDescription, error in
+            if let error = error as NSError? {
+                print(error.localizedDescription)
+            }
+        }
+        
+        guard let oldStore = persistentStoreCoordinator.persistentStore(for: oldStoreURL) else {
+            return
+        }
+        do {
+            try persistentStoreCoordinator.migratePersistentStore(oldStore, to: newStoreURL, options: nil, withType: NSSQLiteStoreType)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        // Delete old store.
+        deleteStore(with: oldStoreURL)
+    }
+    
+    private func deleteStore(with storeURL: URL) {
+        let fileCoordinator = NSFileCoordinator(filePresenter: nil)
+        fileCoordinator.coordinate(writingItemAt: storeURL, options: .forDeleting, error: nil, byAccessor: { url in
+            do {
+                try FileManager.default.removeItem(at: url)
+            } catch {
+                print(error.localizedDescription)
+            }
+        })
+    }
+    
+    func saveContext() {
+        let context = viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // TODO: Adding Exception Handling
+            }
+        }
+    }
+    
+}
